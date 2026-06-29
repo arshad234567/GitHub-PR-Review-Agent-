85ec5a0f39394283abd4e454a1299f99f0afb732
arshad234567 <116151193+arshad234567@users.noreply.github.com>
2026-06-29 16:00:55 +0530

    bootstrap FastAPI application

--- app/config.py
+++ app/config.py
@@ -1,19 +1,22 @@
 from functools import lru_cache
-from fastapi import FastAPI
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    APP_NAME: str = "Github PR review agent"
+    APP_NAME: str = "GitHub PR Review Agent"
+    APP_VERSION: str = "1.0.0"
+
     GITHUB_APP_ID: str
     GITHUB_CLIENT_ID: str
-    GITHUB_WEBHOOK_SECRET :str
+    GITHUB_WEBHOOK_SECRET: str
     GITHUB_PRIVATE_KEY_PATH: str
 
-    model_config = SettingsConfigDict(env_file=".env",extra ="ignore")
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="ignore",
+    )
 
-    @lru_cache
-    def get_settings(self):
-        return Settings()
+@lru_cache
+def get_settings():
+    return Settings()
+settings = get_settings()
 
-    settings = get_settings()
-    
